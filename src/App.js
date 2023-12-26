4d6f16d48a03f7c114f4b0d37c67c00415a48a76
Gokul <togokul7@gmail.com>
2023-12-26 19:35:11 +0530

    deleted unwanted folders

--- src/App.js
+++ src/App.js
@@ -13,31 +13,10 @@ import ComponentA from "./Approach_B/Loader/Content/ComponentA";
 function App() {
   return (
     <div className="App">
-      {/* APPROACH A  */}
-
       {/* <CardBox/> */}
       {/* <CardCircle /> */}
       {/* <NewListingPageHorizontal /> */}
       {/* <NewListingPageVertical /> */}
-
-
-
-      {/* APPROACH B */}
-      {/* <div style={{display: 'flex', justifyContent: 'center', margin: 'auto', top: '40%'}}>
-          <ShimmerComponent
-            isLoading={true}
-            shimmerOrder={[
-              { type: "box", count: 1, width: 260, height: 260 },
-              { type: "lines", count: 1, width: 400, height: 10 },
-              { type: "lines", count: 1, width: 400, height: 10 },
-              { type: "lines", count: 1, width: 200, height: 10 },
-              { type: "photo", count: 1, width: 600, height: 200},
-              { type: "photoCircular", count: 1, diameter: 200, alignment: 'center'},
-            ]}
-          >
-              <div><ComponentA /></div>
-          </ShimmerComponent>
-      </div> */}
     </div>
   );
 }
